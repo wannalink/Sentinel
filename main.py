38e73403a6e471e6f31a78aa51c24612c8046ede
wannalink <roflmao@bk.ru>
2022-12-11 23:24:19 +0300

    no webserver

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from CWebSocket import initialize_websocket
+
 from Schema import create_database
 from threading import Thread
 from commands import bot
 from os import environ
 import logging
-from webserver import keep_alive
+from dotenv import load_dotenv
+# from webserver import keep_alive
 
+load_dotenv()
 logging.basicConfig(filename='tmp/sentinel.log', level=logging.ERROR,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
@@ -26,4 +29,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-keep_alive()
+# keep_alive()
